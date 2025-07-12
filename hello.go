ae56a888d5e5c5fee4e8145912aff36129fddfb7
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-07-12 21:02:16 +0100

    We now replace a blank name with "World"

--- hello.go
+++ hello.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(s string) string {
-	const englishHelloPrefix = "Hello, "
+	if s == "" {
+		s = "world"
+	}
 	return englishHelloPrefix + s + "!"
 }
 
--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Geir")
-	want := "Hello, Geir!"
+	t.Run("says hello to people", func(t *testing.T) {
+		got := Hello("Geir")
+		want := "Hello, Geir!"
+		assertCorrectGreeting(t, got, want)
+	})
+	t.Run("says hello to the world when given an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCorrectGreeting(t, got, want)
+
+	})
+}
+
+func assertCorrectGreeting(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Hello() = %q; want %q", got, want)
 	}
