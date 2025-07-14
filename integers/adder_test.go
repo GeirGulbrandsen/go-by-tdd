7d8d7e1447ea63da3169643032cdf30d28d9fa15
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-07-14 22:40:49 +0100

    Add example

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,10 +1,18 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	if sum != 4 {
 		t.Errorf("Expected 4 but got %d", sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum) // Output: 6
 }
