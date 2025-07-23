f689871813aeac921bf43c6f8b33f2e7bc4d553c
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-07-23 12:59:04 +0200

    Refactor

--- arrays/sumalltails_test.go
+++ arrays/sumalltails_test.go
@@ -6,28 +6,30 @@ import (
 )
 
 func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
 	t.Run("can sum the tail of one slice", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2})
 		want := []int{2}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %d want %d", got, want)
-		}
+		checkSums(t, got, want)
+
 	})
 	t.Run("can sum the tail of three slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{3, 4}, []int{5, 6})
 		want := []int{2, 4, 6}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %d want %d", got, want)
-		}
+		checkSums(t, got, want)
+
 	})
 	t.Run("can handle empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5, 6})
 		want := []int{0, 15}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %d want %d", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
