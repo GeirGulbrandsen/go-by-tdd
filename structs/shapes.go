8b2b58218c52054db527985c65954a787ca4be7f
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-07-26 20:35:01 +0100

    Refactor

---
+++ structs/shapes.go
@@ -0,0 +1,9 @@
+package structs
+
+func Perimeter(f1, f2 float64) float64 {
+	return 2 * (f1 + f2)
+}
+
+func Area(f1, f2 float64) float64 {
+	return f1 * f2
+}
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -10,6 +10,10 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func Perimeter(f1, f2 float64) float64 {
-	return 2 * (f1 + f2)
+func TestArea(t *testing.T) {
+	got := Area(4, 5)
+	want := 20.0
+	if got != want {
+		t.Errorf("got = %v; want %v", got, want)
+	}
 }
