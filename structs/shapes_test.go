ee98bee73840b528ed31e3f56ce2e3ea9cadfc1f
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-07-27 12:11:55 +0100

    Introduced methods to calculate Area of differnet shapes

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,11 +13,19 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	t.Run("calculate area of rectangle", func(t *testing.T) {
-		r := Rectangle{4.0, 5.5}
-		got := Area(r)
-		want := 22.0
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
 		if got != want {
-			t.Errorf("got = %v; want %v", got, want)
+			t.Errorf("got = %g; want %g", got, want)
+		}
+	})
+	t.Run("calculate area of circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got = %g; want %g", got, want)
 		}
 	})
 }
