b342d5cc70fd770d25f2fd6b78d1791d224711d8
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-07-27 12:00:04 +0100

    Refactor

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,7 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	r := Rectangle{Width: 4.5, Height: 5.5}
+	r := Rectangle{4.5, 5.5}
 	got := Perimeter(r)
 	want := 20.0
 	if got != want {
@@ -12,10 +12,12 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	r := Rectangle{Width: 4, Height: 5}
-	got := Area(r)
-	want := 20.0
-	if got != want {
-		t.Errorf("got = %v; want %v", got, want)
-	}
+	t.Run("calculate area of rectangle", func(t *testing.T) {
+		r := Rectangle{4.0, 5.5}
+		got := Area(r)
+		want := 22.0
+		if got != want {
+			t.Errorf("got = %v; want %v", got, want)
+		}
+	})
 }
